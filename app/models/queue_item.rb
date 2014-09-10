a1599fb41e55316da9a4827f925369a9310f16c7
aj <ajjonez@icloud.com>
2014-09-09 22:33:49 -0500

    W3 Work: error on last spec--RecordNotFound

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -8,23 +8,25 @@ class QueueItem < ActiveRecord::Base
 	validates_numericality_of :position, {only_integer: true}
 
 	def rating
-		review = Review.where(video_id: video_id, user_id: user_id).first
 		review.rating if review
 	end
 
 	def rating=(new_rating)
-		review = Review.where(video_id: video_id, user_id: user_id).first
 		if review
 			review.update_column(:rating, new_rating)
 		else
 			review = Review.new(user: user, video: video, rating: new_rating)
 			review.save(validate: false)
 		end
-		
 	end
 
 	def category_name
 		category.name
 	end
 
+	private
+
+	def review
+		@review ||= Review.where(video_id: video.id, user_id: user.id).first
+	end
 end
