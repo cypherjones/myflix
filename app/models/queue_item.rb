532f77f0abe2c9c7500434256a9c6c7a551ff3b8
aj <ajjonez@icloud.com>
2014-09-11 22:24:33 -0500

    W3 work: changing indentions

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,32 +1,32 @@
 class QueueItem < ActiveRecord::Base
-	belongs_to :user
-	belongs_to :video
+  belongs_to :user
+  belongs_to :video
 
-	delegate :category, to: :video 
-	delegate :title, to: :video, prefix: :video
+  delegate :category, to: :video 
+  delegate :title, to: :video, prefix: :video
 
-	validates_numericality_of :position, {only_integer: true}
+  validates_numericality_of :position, {only_integer: true}
 
-	def rating
-		review.rating if review
-	end
+  def rating
+    review.rating if review
+  end
 
-	def rating=(new_rating)
-		if review
-			review.update_column(:rating, new_rating)
-		else
-			review = Review.new(user: user, video: video, rating: new_rating)
-			review.save(validate: false)
-		end
-	end
+  def rating=(new_rating)
+    if review
+      review.update_column(:rating, new_rating)
+    else
+      review = Review.new(user: user, video: video, rating: new_rating)
+      review.save(validate: false)
+    end
+  end
 
-	def category_name
-		category.name
-	end
+  def category_name
+    category.name
+  end
 
-	private
+  private
 
-	def review
-		@review ||= Review.where(video_id: video.id, user_id: user.id).first
-	end
+  def review
+    @review ||= Review.where(video_id: video.id, user_id: user.id).first
+  end
 end
